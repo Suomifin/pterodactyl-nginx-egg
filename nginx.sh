dc4894247d830e5df26cf5a55bef0793f52b51e7
Suomifin <76266692+Suomifin@users.noreply.github.com>
2024-03-30 23:52:42 +0200

    Custom version

--- nginx.sh
+++ nginx.sh
@@ -2,9 +2,12 @@
 rm -rf /home/container/tmp/*
 PHP_VERSION=$(cat "/home/container/php_version.txt")
 
-echo "[Docker] Starting PHP-FPM"
+echo "[Server] Käynnistetään PHP-FPM ${PHP_VERSION}"
 php-fpm$PHP_VERSION -c /home/container/php/php.ini --fpm-config /home/container/php/php-fpm.conf --daemonize > /dev/null 2>&1
 
-echo "[Docker] Starting NGINX"
-echo "[Docker] Services successfully launched"
+echo "[Server] Käynnistetään NGINX"
+echo "[Server] Palvelut käynnistetty onnistuneesti!"
+echo "==========================="
+echo "[Suomifin.cloud] System hosted by: Suomifin.eu"
+echo "==========================="
 nginx -c /home/container/nginx/nginx.conf -p /home/container
